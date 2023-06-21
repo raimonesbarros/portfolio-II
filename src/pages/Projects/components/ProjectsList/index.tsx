1b657e62b9ed61c7451531d04b8d115c24803b4a
Raimones Barros <raimonesrg3@gmail.com>
2023-06-21 20:00:37 -0300

    feat: create projects list

---
+++ src/pages/Projects/components/ProjectsList/index.tsx
@@ -0,0 +1,16 @@
+import { Project } from '../../Project'
+import { ProjectsListContainer } from './styles'
+
+export function ProjectsList() {
+  return (
+    <ProjectsListContainer>
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+    </ProjectsListContainer>
+  )
+}
---
+++ src/pages/Projects/components/ProjectsList/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components'
+
+export const ProjectsListContainer = styled.div`
+  width: 80%;
+  max-height: calc(100vh - 10rem);
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  border-radius: 8px;
+  overflow: auto;
+`
