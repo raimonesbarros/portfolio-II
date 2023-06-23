6fb7a931f1480e84eb95fa4b68550790455eed35
Raimones Barros <raimonesrg3@gmail.com>
2023-06-23 14:33:45 -0300

    feat: make dinamic project viewer

--- src/pages/Projects/components/Viewer/index.tsx
+++ src/pages/Projects/components/Viewer/index.tsx
@@ -1,29 +1,40 @@
 import { Buttons, ViewerContainer, ViewerContent } from './styles'
-import coffee from '../../../../assets/coffee.png'
 
-export function Viewer() {
+interface ViewerProps {
+  image: string
+  title: string
+  tags: string[]
+  fullDescription: string
+  deploy: string
+  repository: string
+}
+
+export function Viewer({
+  image,
+  title,
+  tags,
+  fullDescription,
+  deploy,
+  repository,
+}: ViewerProps) {
   return (
     <ViewerContainer>
       <ViewerContent>
-        <img src={coffee} alt="" />
+        <img src={image} alt="" />
         <Buttons>
-          <a href="https://www.google.com" target="_blank" rel="noreferrer">
+          <a href={deploy} target="_blank" rel="noreferrer">
             Ver mais
           </a>
-          <a href="https://www.google.com" target="_blank" rel="noreferrer">
+          <a href={repository} target="_blank" rel="noreferrer">
             Projeto
           </a>
         </Buttons>
-        <h2>Coffee Delivery</h2>
+        <h2>{title}</h2>
         <div>
-          <span>reactJS</span>
+          <span>{tags[0]}</span>
+          <span>{tags[1]}</span>
         </div>
-        <p>
-          Aplicação de loja virtual - cafeteria - simulando compra online com
-          possibilidade de navegar pela loja, adicionar produtos ao ao carrinho,
-          alterar quantidade, remover itens do carrinho e finalizar pedido
-          informando endereço de entrega e forma de pagamento
-        </p>
+        <p>{fullDescription}</p>
       </ViewerContent>
     </ViewerContainer>
   )
