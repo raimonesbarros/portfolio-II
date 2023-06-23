622cd409e13eb867cdd84e2af16a5bf13b609c51
Raimones Barros <raimonesrg3@gmail.com>
2023-06-23 18:17:03 -0300

    style: remove focus style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -7,11 +7,6 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  :focus {
-    outline: 0;
-    box-shadow: 0 0 0 2px ${(p) => p.theme.primary};
-  }
-
   html, body {
     background: ${(p) => p.theme.background};
 
